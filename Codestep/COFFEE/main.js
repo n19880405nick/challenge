0d04afc264995449c6b190132012f64262963365
n19880405nick <102840465+n19880405nick@users.noreply.github.com>
2022-08-15 06:40:26 +0000

    Codestep COFFEE PC.ver finished

---
+++ Codestep/COFFEE/main.js
@@ -0,0 +1,8 @@
+$(function(){
+  $('a[href^="#').click(function(){
+    let href = $(this).attr("href");
+    let target = $(href == "#" || href == "" ? 'html' : href);
+    let position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, 600, "swing");
+  });
+});
