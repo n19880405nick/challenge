0b495c4c652a45924ae3c1e87b491e4dc2c1ea88
n19880405nick <102840465+n19880405nick@users.noreply.github.com>
2022-08-30 05:41:53 +0000

    Codestep Sneakers PC.ver making
    header finished

---
+++ Codestep/Sneakers/main.js
@@ -0,0 +1,21 @@
+$(function(){
+	$('a[href^="#').click(function(){
+		let href = $(this).attr("href");
+		let target = $(href == "#" || href == "" ? 'html' : href);
+		let position = target.offset().top;
+		$("html, body").animate({scrollTop:position}, 600, "swing");
+	});
+	$('.hamburger').on('click',function(){
+		if($('header').hasClass('open')){
+			$('header').removeClass('open');
+		}else{
+			$('header').addClass('open');
+		}
+	});
+	$('nav').on('click',function(){
+		$('header').removeClass('open');
+	});
+	$('.mask').on('click',function(){
+		$('header').removeClass('open');
+	});
+});
