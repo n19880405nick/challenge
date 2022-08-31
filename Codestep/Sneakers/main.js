86653229c4d08f946befc85ddb7a1ad83e37af5b
n19880405nick <102840465+n19880405nick@users.noreply.github.com>
2022-08-31 04:57:32 +0000

    Codestep Sneakers PC.ver finished

--- Codestep/Sneakers/main.js
+++ Codestep/Sneakers/main.js
@@ -2,7 +2,7 @@ $(function(){
 	$('a[href^="#').click(function(){
 		let href = $(this).attr("href");
 		let target = $(href == "#" || href == "" ? 'html' : href);
-		let position = target.offset().top;
+		let position = target.offset().top - 100;
 		$("html, body").animate({scrollTop:position}, 600, "swing");
 	});
 	$('.hamburger').on('click',function(){
@@ -18,4 +18,18 @@ $(function(){
 	$('.mask').on('click',function(){
 		$('header').removeClass('open');
 	});
+	// slider
+	$('.slider').slick({
+		infinite: true,
+  	slidesToScroll: 1,
+  	slidesToShow: 1,
+		centerMode: true,
+  	variableWidth: true
+	});
+	// inview
+	$('.feature-image').on('inview',function(event, isInView, visiblePartX, visiblePartY){
+    if (isInView){
+      $(this).addClass('is-inview');
+    }
+  });
 });
