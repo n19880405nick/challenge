0ec15f0155633b1230f13982d3adb3af89143105
n19880405nick <102840465+n19880405nick@users.noreply.github.com>
2022-09-05 03:45:00 +0000

    Codestep FA-EXHIBITION PC.ver making
    #information #gallery finished

--- Codestep/FA EXHIBITION/main.js
+++ Codestep/FA EXHIBITION/main.js
@@ -1,4 +1,5 @@
 $(function(){
+  // ハンバーガーメニュー
   $('.humberger').on('click', function(){
     if($('header').hasClass('open')){
       $('header').removeClass('open');
@@ -6,5 +7,22 @@ $(function(){
       $('header').addClass('open');
     }
   });
-
+  // ヘッダの表示
+  $(window).on('scroll',function(){
+		if($(window).scrollTop() > 520){
+			$('header').fadeIn();
+		}else{
+			$('header').fadeOut();
+		}
+	});
+  // 	メインビジュアルのサイズ変更
+  $(window).on('scroll',function(){
+		$('#top img').css('width', 100/3 + $(window).scrollTop()/10 + '%');
+	});
+  // 	フェードイン
+  $('.is-inview').on('inview',function(event, isInView, visiblePartX, visiblePartY){
+    if (isInView){
+      $(this).addClass('is-inview-fadein');
+    }
+  });
 });
