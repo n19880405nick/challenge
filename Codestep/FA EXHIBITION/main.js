5889c3204dbb9666716d90d83c76a0e326a17478
n19880405nick <102840465+n19880405nick@users.noreply.github.com>
2022-09-06 05:42:14 +0000

    Codestep FA-EXHIBITION PC.ver finished

--- Codestep/FA EXHIBITION/main.js
+++ Codestep/FA EXHIBITION/main.js
@@ -7,6 +7,9 @@ $(function(){
       $('header').addClass('open');
     }
   });
+  $('nav').on('click', function(){
+	   $('header').removeClass('open');
+	});
   // ヘッダの表示
   $(window).on('scroll',function(){
 		if($(window).scrollTop() > 520){
@@ -14,6 +17,13 @@ $(function(){
 		}else{
 			$('header').fadeOut();
 		}
+	});
+  // 	スムーズスクロール
+  $('a[href^="#').click(function(){
+		let href = $(this).attr("href");
+		let target = $(href == "#" || href == "" ? 'html' : href);
+		let position = target.offset().top;
+		$("html, body").animate({scrollTop:position}, 600, "swing");
 	});
   // 	メインビジュアルのサイズ変更
   $(window).on('scroll',function(){
@@ -23,6 +33,27 @@ $(function(){
   $('.is-inview').on('inview',function(event, isInView, visiblePartX, visiblePartY){
     if (isInView){
       $(this).addClass('is-inview-fadein');
+    }
+  });
+  // アクセスの表示と非表示
+  $('#access .title').on('inview',function(event, isInView, visiblePartX, visiblePartY){
+    if (isInView){
+      $('.bg').fadeIn();
+    }else{
+      $('.bg').fadeOut();
+    }
+  });
+  $('#contact .title').on('inview',function(event, isInView, visiblePartX, visiblePartY){
+    if (isInView){
+      $('.bg').fadeOut();
+    }
+  });
+  // サイドバーの表示と非表示
+  $('#gallery').on('inview',function(event, isInView, visiblePartX, visiblePartY){
+    if (isInView){
+      $('#sidebar').addClass('is-inview-side');
+    }else{
+      $('#sidebar').removeClass('is-inview-side');
     }
   });
 });
